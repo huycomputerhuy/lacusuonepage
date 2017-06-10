d8256cb94f4a1d75b31238cf948b486b257199f9
Huy Vo <huycomputerhuy@gmail.com>
2017-06-10 20:38:16 +0700

    add more config about color and font

--- lacusuonepage/template-parts/alo-pop.php
+++ lacusuonepage/template-parts/alo-pop.php
@@ -1,5 +1,5 @@
 <div class="quick-alo-phone quick-alo-green quick-alo-show" id="quick-alo-phoneIcon" style="right: 10px; top: 20%;">
-    <a  href="tel:0901463986" data-toggle="modal" data-target="#callback" title="Gọi chúng tôi ngay!">
+    <a  href="tel:<?php echo get_theme_mod("lacusu_phone_num_alo")?>" data-toggle="modal" data-target="#callback" title="<?php echo get_theme_mod("lacusu_massgage_alo") ?>">
         <div class="quick-alo-ph-circle"></div>
         <div class="quick-alo-ph-circle-fill"></div>
         <div class="quick-alo-ph-img-circle"></div>
